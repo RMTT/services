94fa079c54b7e130737e89fc4bc36f856e23fa80
RMT <iamrmttt@gmail.com>
2026-06-16 15:44:37 +0800

    update juicefs csi

--- apps/kube-system/juicefs-csi/README.md
+++ apps/kube-system/juicefs-csi/README.md
@@ -8,3 +8,7 @@ JuiceFS is a distributed POSIX file system built on top of Redis and S3. The CSI
 - Helm Chart Repository: https://juicedata.github.io/charts/
 - Namespace: `kube-system`
 - The `controller` component uses a nodeSelector to run specifically on `homeserver` (the node labels), while the `node` (DaemonSet) will run on all nodes where mounts are requested.
+
+## Notes
+
+This Configuration use `pathPattern: "${.pvc.namespace}-${.pvc.name}"`, it' ok for single cluster usage, but it's cannot be used for multi-cluster with same backend.
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              kubectl
+              kubectl-cnpg
+            ];
+          };
+        }
+      );
+    };
+}
